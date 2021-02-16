af771df840157acfcc78c64b7e04bd8f66f03de1
Bartłomiej Drążkiewicz <drazkiewiczbart@icloud.com>
2021-02-16 18:28:31 +0100

    Refactor files

--- config/config.js
+++ config/config.js
@@ -1,28 +1,13 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '/.env') });
 
-const dbURI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-const dbConfig = {
-  user: process.env.DB_USER,
-  pass: process.env.DB_USER_PWD,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-const sessionConfig = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  httpOnly: true,
-  cookie: {
-    secure: false,
-  },
-};
-
 module.exports = {
   port: process.env.PORT,
   host: process.env.HOST,
-  dbURI,
-  dbConfig,
-  sessionConfig,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbUserPwd: process.env.DB_USER_PWD,
+  sessionSecret: process.env.SESSION_SECRET,
 };
--- config/index.js
+++ config/index.js
@@ -1,31 +1,78 @@
+// Import modules
 const http = require('http');
 const express = require('express');
 const app = express();
 const server = http.createServer(app);
+const expressSession = require('express-session');
 const mongoose = require('mongoose');
-const session = require('express-session');
+const MongoStore = require('connect-mongo')(expressSession);
 const io = require('socket.io')(server);
 require('./socket-server-side')(io);
-// const MongoDBStore = require('connect-mongodb-session')(session);
 const path = require('path');
-const { port, host, dbURI, dbConfig, sessionConfig } = require('./config');
+const {
+  port,
+  host,
+  dbHost,
+  dbPort,
+  dbName,
+  dbUser,
+  dbUserPwd,
+  sessionSecret,
+} = require('./config');
 
+// App set
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-app.use(session(sessionConfig));
+// App use
 app.use(express.static(path.join(__dirname, '../public')));
+const session = expressSession({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: true,
+  httpOnly: true,
+  cookie: {
+    secure: false,
+  },
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+});
+app.use(session);
+io.use((socket, next) => {
+  session(socket.request, {}, next);
+});
 
+// Routers
 require('../routers/chat-room-router')(app);
 
+// Database and server
 (async () => {
   try {
+    const dbURI = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+    const dbConfig = {
+      user: dbUser,
+      pass: dbUserPwd,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
     await mongoose.connect(dbURI, dbConfig);
-    console.log('Database connection status: connected');
+    console.log('Start: database connected.');
     server.listen(port, host, () => {
-      console.log(`Server is listening on ${host}:${port}`);
+      console.log(`Start: server is listening on ${host}:${port}.`);
     });
   } catch (err) {
-    console.error(err);
+    console.error(`Start: database or server error. ${err}.`);
   }
 })();
+
+// Events
+mongoose.connection.on('disconnected', () => {
+  console.log('Database disconnected.');
+});
+
+mongoose.connection.on('reconnected', () => {
+  console.log('Database reconnected.');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Database error. ${err}.`);
+});
