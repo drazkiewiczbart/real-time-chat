961a87baa40061703744ddac26a9e1c3dc7f6007
Bartłomiej Drążkiewicz <drazkiewiczbart@icloud.com>
2021-02-18 19:19:37 +0100

    Add basic page layout, rebuild socket implementation

--- public/javascripts/socket-io-client.js
+++ public/javascripts/socket-io-client.js
@@ -1,15 +1,37 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const ul = document.querySelector('ul');
 const socket = io();
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  socket.emit('clientMsg', input.value);
+$('form').submit((event) => {
+  event.preventDefault();
+  socket.emit('clientMsg', $('input').val());
 });
 
 socket.on('serverMsg', (serverMsg) => {
-  const li = document.createElement('li');
-  li.innerText = serverMsg;
-  ul.appendChild(li);
+  const [author, message, data, time] = serverMsg;
+  const templateMessage = `
+  <div class="row py-3">
+    <div class="col message">
+      <div class="row mb-1 pt-3 px-3">
+        <div class="col message-data">
+          <p>
+            <span class="data">${data}</span>
+            <span class="time">${time}</span>
+            <span class="author">${author}</span> says:
+          </p>
+        </div>
+      </div>
+      <div class="row pb-3 px-3">
+        <div class="col message-content">
+          <p>
+            ${message}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  $(templateMessage).appendTo($('#messages-wrapper'));
+  $('#messages-wrapper').scrollTop(
+    $('#messages-wrapper')[0].scrollHeight -
+      $('#messages-wrapper')[0].clientHeight,
+  );
 });
